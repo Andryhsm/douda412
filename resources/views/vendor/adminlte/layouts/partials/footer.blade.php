d8582e888c348bbe296ff6a7250983d935d400b8
Andryhsm <andryhsm@gmail.com>
2017-11-20 17:03:02 +0100

    Projet Stian fin d'année

---
+++ resources/views/vendor/adminlte/layouts/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right 
+    <div class="pull-right hidden-xs">
+        <a href="https://github.com/acacha/adminlte-laravel"></a><b>admin-lte-laravel</b></a>. {{ trans('adminlte_lang::message.descriptionpackage') }}
+    </div>
+    <!-- Default to the left -->
+    <center>
+    <strong>Copyright &copy; 2017 <a href="http://medsoft-sante.fr">Medsoft-sante.fr</a>.</strong> {{ trans('adminlte_lang::message.createdby') }} <a href="#">Msoft Madagascar</a><!-- . {{ trans('adminlte_lang::message.seecode') }} <a href="https://github.com/acacha/adminlte-laravel">Github</a> -->
+    </center>
+</footer>
